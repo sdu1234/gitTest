f42379f1e3f8bfa96a2f832ffc8efddfba46a09b
sdu1808 <1376702061@qq.com>
2023-03-23 20:18:40 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/GitDemo.java
+++ src/main/java/com/atguigu/GitDemo.java
@@ -4,5 +4,6 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("hello Git !!!");
         System.out.println("hello Git222 !!!");
+        System.out.println("hello Git444 !!!");
     }
 }
